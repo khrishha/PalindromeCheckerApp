c3bfc8b246287050d86cd9bc012ed3fcecb2017d
khrishha <ks8357@srmist.edu.in>
2026-03-02 15:50:26 +0530

    Khrisha added Stack-Based Palindrome Checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,41 +1,20 @@
-import java.util.Scanner;
-
+import java.util.Scanner; import java.util.Stack;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("====================================="); System.out.println(" Palindrome Checker App");
-        System.out.println("=====================================");
-
-
-
-        System.out.println("UC4: Character Array Based Palindrome Check");
-        System.out.println("-------------------------------------"); Scanner sc = new Scanner(System.in);
+        System.out.println("====================================="); System.out.println("UC5: Stack-Based Palindrome Checker"); System.out.println("-------------------------------------"); Scanner sc = new Scanner(System.in);
         System.out.print("Enter a word or phrase: "); String input = sc.nextLine();
-        String cleaned = input.replaceAll("\\s+", "").toLowerCase(); char[] chars = cleaned.toCharArray();
-        int start = 0;
-        int end = chars.length - 1; boolean isPalindrome = true;
-
-
-
-
-        while (start < end) {
-            if (chars[start] != chars[end]) { isPalindrome = false;
-                break;
-            }
-            start++; end--;
+        String cleaned = input.replaceAll("\\s+", "").toLowerCase(); Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < cleaned.length(); i++) { stack.push(cleaned.charAt(i));
         }
-        if (isPalindrome) {
+        String reversed = "";
+        while (!stack.isEmpty()) { reversed += stack.pop();
+        }
+        if (cleaned.equals(reversed)) {
             System.out.println("✅ \"" + input + "\" is a palindrome!"); } else {
             System.out.println("❌ \"" + input + "\" is NOT a palindrome.");
         }
-
-
-
-        System.out.println("-------------------------------------");
-
-
-
-        System.out.println("Program execution completed.");
+        System.out.println("-------------------------------------"); System.out.println("Program execution completed.");
         sc.close();
-
     }
 }
