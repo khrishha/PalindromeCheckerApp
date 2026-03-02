ead27ad122a4704a27d49eee36e217b3e2458c59
khrishha <ks8357@srmist.edu.in>
2026-03-02 15:26:59 +0530

    Khrisha added UC3: PalindromeCheck Using String Reverse

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         System.out.println("====================================="); System.out.println(" Palindrome Checker App");
-        System.out.println("====================================="); System.out.println("UC3: User Input & Dynamic Palindrome Check"); System.out.println("-------------------------------------"); Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a word or phrase: "); String input = sc.nextLine();
-        String cleanedInput = input.replaceAll("\\s+", "").toLowerCase(); String reversed = "";
-        for (int i = cleanedInput.length() - 1; i >= 0; i--) { reversed += cleanedInput.charAt(i);
+        System.out.println("====================================="); System.out.println("UC3: Palindrome Check Using String Reverse"); System.out.println("-------------------------------------"); Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word or phrase: "); String original = sc.nextLine();
+        String cleaned = original.replaceAll("\\s+", ""); String reversed = "";
+        for (int i = cleaned.length() - 1; i >= 0; i--) { reversed += cleaned.charAt(i);
         }
-        if (cleanedInput.equals(reversed)) {
-            System.out.println("✅\"" + input + "\" is a palindrome!"); } else {
-            System.out.println("❌\"" + input + "\" is NOT a palindrome.");
+        if (cleaned.equalsIgnoreCase(reversed)) {
+            System.out.println("✅\"" + original + "\" is a palindrome!"); } else {
+            System.out.println("❌\"" + original + "\" is NOT a palindrome.");
         }
         System.out.println("-------------------------------------"); System.out.println("Program execution completed.");
         sc.close();
     }
 }
+
